6b03b013614ffac8a1ed38080caf8b669bbb444f
Eric Woroshow <eric@ericw.ca>
2010-07-08 17:36:41 -0700

    Trying a new style.

---
+++ Strategy/Interface/BackgroundView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Strategy.Gameplay;
+
+
+namespace Strategy.Interface
+{
+    /// <summary>
+    /// Shows the background.
+    /// </summary>
+    public class BackgroundView
+    {
+        public BackgroundView(InterfaceContext context)
+        {
+            _background = context.Content.Load<Texture2D>("Background");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_background, Vector2.Zero, Color.White);
+        }
+
+        private Texture2D _background;
+    }
+}
