65ae61b09011a1bb354c2657fac5168231969bc6
Eric Woroshow <eric@ericw.ca>
2010-07-25 22:41:44 -0700

    Imported screen management code from my XNA library.

---
+++ Strategy/Library/Screen/Screen.cs
@@ -0,0 +1,203 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Strategy.Library.Input;
+
+namespace Strategy.Library.Screen
+{
+    /// <summary>
+    /// The screen transition state.
+    /// </summary>
+    public enum ScreenState
+    {
+        Active,
+        TransitionOn,
+        TransitionOff,
+        Inactive
+    }
+
+    /// <summary>
+    /// A screen.
+    /// </summary>
+    public class Screen
+    {
+        /// <summary>
+        /// The current state of the screen.
+        /// </summary>
+        public ScreenState State { get; protected set; }
+
+        /// <summary>
+        /// Occurs when this screen changes state.
+        /// </summary>
+        public event EventHandler<EventArgs> StateChanged;
+
+        /// <summary>
+        /// The screen stack on which this screen is placed.
+        /// </summary>
+        public ScreenStack Stack { get; internal set; }
+
+        /// <summary>
+        /// If this screen should show the screens beneath it in the stack.
+        /// </summary>
+        public bool ShowBeneath { get; protected set; }
+
+        /// <summary>
+        /// The time, in seconds, for this screen to go from inactive to active.
+        /// </summary>
+        protected float TransitionOnTime { get; set; }
+
+        /// <summary>
+        /// The time, in seconds, for this screen to go from active to inactive.
+        /// </summary>
+        protected float TransitionOffTime { get; set; }
+
+        /// <summary>
+        /// Creates a new screen.
+        /// </summary>
+        public Screen()
+        {
+            State = ScreenState.Inactive;
+            ShowBeneath = false;
+        }
+
+        /// <summary>
+        /// Updates this screen.
+        /// </summary>
+        /// <param name="time">The elapsed time, in seconds, since the last update.</param>
+        public void Update(float time)
+        {
+            switch (State)
+            {
+                case ScreenState.Active:
+                    UpdateActive(time);
+                    break;
+                case ScreenState.Inactive:
+                    UpdateInactive(time);
+                    break;
+                case ScreenState.TransitionOn:
+                    _transitionElapsed += time;
+                    if (_transitionElapsed > TransitionOnTime)
+                    {
+                        OnStateChanged(ScreenState.Active);
+                    }
+                    UpdateTransitionOn(time, MathHelper.Clamp(_transitionElapsed / TransitionOnTime, 0f, 1f), _transitionStack);
+                    break;
+                case ScreenState.TransitionOff:
+                    _transitionElapsed += time;
+                    if (_transitionElapsed > TransitionOffTime)
+                    {
+                        OnStateChanged(ScreenState.Inactive);
+                    }
+                    UpdateTransitionOff(time, MathHelper.Clamp(_transitionElapsed / TransitionOffTime, 0f, 1f), _transitionStack);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Draws this screen.
+        /// </summary>
+        public virtual void Draw()
+        {
+        }
+
+        /// <summary>
+        /// Transitions this screen to the active state.
+        /// </summary>
+        /// <param name="pushed">True if the screen was pushed on the top of the stack,
+        /// false if it was shown by a newly popped screen.</param>
+        protected internal virtual void Show(bool pushed)
+        {
+            if (State == ScreenState.Active || State == ScreenState.TransitionOn)
+            {
+                return; // nothing to do
+            }
+            if (TransitionOnTime > 0)
+            {
+                OnStateChanged(ScreenState.TransitionOn);
+                _transitionElapsed = 0f;
+                _transitionStack = pushed;
+            }
+            else
+            {
+                OnStateChanged(ScreenState.Active);
+            }
+        }
+
+        /// <summary>
+        /// Transitions this screen to the inactive state.
+        /// </summary>
+        /// <param name="popped">True if the screen was popped from the stack,
+        /// false if it is obscured by a newly pushed screen.</param>
+        protected internal virtual void Hide(bool popped)
+        {
+            if (State == ScreenState.Inactive || State == ScreenState.TransitionOff)
+            {
+                return; // nothing to do
+            }
+            if (TransitionOffTime > 0)
+            {
+                OnStateChanged(ScreenState.TransitionOff);
+                _transitionElapsed = 0f;
+                _transitionStack = popped;
+            }
+            else
+            {
+                OnStateChanged(ScreenState.Inactive);
+            }
+        }
+
+        /// <summary>
+        /// Updates this screen when it is active.
+        /// </summary>
+        /// <param name="time">The elapsed time, in seconds, since the last update.</param>
+        protected virtual void UpdateActive(float time)
+        {
+        }
+
+        /// <summary>
+        /// Updates this scren when it is inactive.
+        /// </summary>
+        /// <param name="time">The elapsed time, in seconds, since the last update.</param>
+        protected virtual void UpdateInactive(float time)
+        {
+        }
+
+        /// <summary>
+        /// Updates this screen when it is transitioning on.
+        /// </summary>
+        /// <param name="time">The elapsed time, in seconds, since the last update.</param>
+        /// <param name="progress">The percentage completion of the transition.</param>
+        /// <param name="pushed">True if the screen was pushed on the stack; otherwise, false.</param>
+        protected virtual void UpdateTransitionOn(float time, float progress, bool pushed)
+        {
+        }
+
+        /// <summary>
+        /// Updates this screen when it is transitioning off.
+        /// </summary>
+        /// <param name="time">The elapsed time, in seconds, since the last update.</param>
+        /// <param name="progress">The percentage completion of the transition.</param>
+        /// <param name="popped">True if the screen was popped from the stack; otherwise, false.</param>
+        protected virtual void UpdateTransitionOff(float time, float progress, bool popped)
+        {
+        }
+
+        /// <summary>
+        /// Notifies listeners that the state of this screen changed.
+        /// </summary>
+        /// <param name="newState">The new state of the screen.</param>
+        protected virtual void OnStateChanged(ScreenState newState)
+        {
+            State = newState;
+            if (StateChanged != null)
+            {
+                StateChanged(this, EventArgs.Empty);
+            }
+        }
+
+        private float _transitionElapsed = 0f;
+        private bool _transitionStack;
+    }
+}
---
+++ Strategy/Library/Screen/ScreenStack.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Strategy.Library.Extensions;
+using Strategy.Library.Input;
+
+namespace Strategy.Library.Screen
+{
+    /// <summary>
+    /// A stack of screens. The stack is drawn from bottom to top. Only the top screen is active and updated.
+    /// </summary>
+    public class ScreenStack : DrawableGameComponent
+    {
+        /// <summary>
+        /// The screen at the top of the stack.
+        /// </summary>
+        public Screen ActiveScreen
+        {
+            get
+            {
+                return (_stackScreens.Count > 0) ? _stackScreens[_stackScreens.Count - 1] : null;
+            }
+        }
+
+        /// <summary>
+        /// Creates a new stack of screens.
+        /// </summary>
+        /// <param name="game">The game context.</param>
+        public ScreenStack(Game game) : base(game)
+        {
+        }
+
+        /// <summary>
+        /// Updates the active screen.
+        /// </summary>
+        /// <param name="gameTime">A snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            float time = gameTime.GetElapsedSeconds();
+            if (time <= 0f || !Game.IsActive)
+            {
+                return; // discard "empty" updates
+            }
+            _stackScreens.ForEach(screen => screen.Update(time));
+            _poppedScreens.ForEach(screen => screen.Update(time));
+            _poppedScreens.RemoveAll(screen => screen.State == ScreenState.Inactive);
+        }
+
+        /// <summary>
+        /// Draws the visible screens in the stack.
+        /// </summary>
+        /// <param name="gameTime">A snapshot of timing values.</param>
+        public override void Draw(GameTime gameTime)
+        {
+            if (_stackScreens.Count > 0)
+            {
+                // find the bottom opaque screen
+                int bottom = _stackScreens.Count - 1;
+                while (bottom > 0 && _stackScreens[bottom].ShowBeneath)
+                {
+                    bottom--;
+                }
+
+                // draw from the bottom up
+                for (; bottom < _stackScreens.Count; bottom++)
+                {
+                    _stackScreens[bottom].Draw();
+                }
+            }
+
+            // draw the screens transitioning off
+            _poppedScreens.ForEach(s => s.Draw());
+        }
+
+        /// <summary>
+        /// Adds a screen on the top of the stack.
+        /// </summary>
+        /// <param name="screen">The screen to push.</param>
+        public void Push(Screen screen)
+        {
+            Screen active = ActiveScreen;
+            if (active != null)
+            {
+                active.Hide(false);
+            }
+            _stackScreens.Add(screen);
+            screen.Stack = this;
+            screen.Show(true);
+        }
+
+        /// <summary>
+        /// Removes the screen at the top of the stack.
+        /// </summary>
+        /// <returns>The popped screen; null if the stack is empty.</returns>
+        public Screen Pop()
+        {
+            // remove and hide the current active screen
+            Screen active = ActiveScreen;
+            if (active != null)
+            {
+                active.Hide(true);
+                _stackScreens.RemoveAt(_stackScreens.Count - 1);
+                _poppedScreens.Insert(0, active);
+            }
+
+            // show the new active screen
+            active = ActiveScreen;
+            if (active != null)
+            {
+                active.Show(false);
+            }
+            return active;
+        }
+
+        /// <summary>
+        /// Pops all the screens from the stack.
+        /// </summary>
+        public void PopAll()
+        {
+            _stackScreens.ForEach(screen => screen.Hide(true));
+            _poppedScreens.AddRange(_stackScreens);
+            _stackScreens.Clear();
+        }
+
+        private List<Screen> _stackScreens = new List<Screen>();
+        private List<Screen> _poppedScreens = new List<Screen>();
+
+        private SpriteBatch _spriteBatch;
+    }
+}
