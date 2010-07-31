d96d1fc3e8b4dcb045617786f318da6c62bebd11
Eric Woroshow <eric@ericw.ca>
2010-07-31 11:57:14 -0700

    Added stubs for screen flow.

---
+++ Strategy/Interface/Screens/TitleScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Strategy.Library.Screen;
+
+namespace Strategy.Interface.Screens
+{
+    /// <summary>
+    /// Displays the title.
+    /// </summary>
+    public class TitleScreen : Screen
+    {
+        public TitleScreen(StrategyGame game)
+        {
+        }
+
+        public override void Draw()
+        {
+        }
+
+        protected override void UpdateActive(GameTime gameTime)
+        {
+        }
+    }
+}
