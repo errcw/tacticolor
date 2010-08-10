b0442c546e0f9bbcf1fb149707f48098ead67b68
Eric Woroshow <eric@ericw.ca>
2010-08-09 20:59:01 -0700

    Stubbed out an AI input class.

---
+++ Strategy/AI/AIInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+using Strategy.Gameplay;
+using Strategy.Net;
+using Strategy.Library.Input;
+
+namespace Strategy.Interface
+{
+    /// <summary>
+    /// Generates input from a computer player.
+    /// </summary>
+    public class AIInput : ICommandProvider
+    {
+        public AIInput()
+        {
+        }
+
+        /// <summary>
+        /// Updates the input state.
+        /// </summary>
+        public Command Update(int time)
+        {
+            return null;
+        }
+    }
+}
