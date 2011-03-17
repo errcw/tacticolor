58828696568b02db14dc7bffc68b6dd99eea0e82
Eric Woroshow <eric@ericw.ca>
2011-03-16 20:37:13 -0700

    Corrected the assembly name to Tacticolor and added my name for copyright.

--- Strategy/Properties/AssemblyInfo.cs
+++ Strategy/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Strategy")]
-[assembly: AssemblyProduct("Strategy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyTitle("Tacticolor")]
+[assembly: AssemblyProduct("Tacticolor")]
+[assembly: AssemblyDescription("A game of tactics.")]
+[assembly: AssemblyCompany("errcw games")]
 
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCopyright("Copyright © Eric Woroshow 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
